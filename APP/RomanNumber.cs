970b5880118947ad74efb1b7694331aa0d07f471
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-09-06 18:17:30 +0300

    !!

--- APP/RomanNumber.cs
+++ APP/RomanNumber.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace APP
+namespace App
 {
     public class RomanNumber
     {
-        // V - 0.2. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        private static Dictionary<char, int> romanValues = new Dictionary<char, int>
+        public int Value { get; set; }
+        private static Dictionary<char, int> roman_values = new Dictionary<char, int>
         {
             { 'I', 1 },
             { 'V', 5 },
@@ -19,64 +19,35 @@ namespace APP
             { 'D', 500 },
             { 'M', 1000 }
         };
-        public Int32 Value { get; set; }
 
-        public static RomanNumber Parse(string roman)
+        public static RomanNumber Parse(String input)
         {
-            if (String.IsNullOrEmpty(roman))
-                throw new ArgumentException("Empty or null number");
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentException("Empty or NULL input");
 
             int result = 0;
             int prev = 0;
+            int first_digit_index = input.StartsWith("-") ? 1 : 0;
 
-            for (int i = roman.Length - 1; i >= 0; i--)
+            for (int i = input.Length - 1; i >= first_digit_index; i--)
             {
-                int current = romanValues[roman[i]];
+                int current = input[i] switch
+                {
+                    'I' => 1,
+                    'V' => 5,
+                    'X' => 10,
+                    'L' => 50,
+                    'C' => 100,
+                    'D' => 500,
+                    'M' => 1000,
+                    'N' => 0,
+                    _ => throw new ArgumentException(" 'A'B' ")
+                };
 
-                if (current < prev)
-                    result -= current;
-                else
-                    result += current;
+                result += (current < prev) ? -current : current;
                 prev = current;
             }
-            return new RomanNumber() { Value = result };
+            return new RomanNumber() { Value = result * (1 - (first_digit_index << 1)) };
         }
-
-        // V - 0.1. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        //
-        //private static Dictionary<int, string> _roman_numerals;  // Contains Roman and Arabic numerals.
-        //
-        //public Int32 Value { get; set; }
-        //
-        //static RomanNumber()
-        //{
-        //    _roman_numerals = new Dictionary<int, string>()
-        //    {
-        //        {1, "I"},
-        //        {2, "II"},
-        //        {5, "V"}
-        //    };
-        //}
-        //
-        //public static RomanNumber Parse(string input)  // Converting a Roman notation for a number (represented as a string) to an
-        //{                                             // integer (Int32) and returning that integer as an object of type RomanNumber.
-        //    int result = 0;  // Contain the result of converting a Roman numeral to an integer.
-        //    
-        //    for (int i = 0; i < input.Length; i++)
-        //    {
-        //         // This part of the code checks if the current character is less than the next character in Roman notation.
-        //        // This checks for cases where a Roman numeral is written in a form where the smaller digit comes before the larger one.
-        //        string current_symbol = input[i].ToString();
-        //    
-        //        if (i < input.Length - 1 && _roman_numerals.ContainsValue(current_symbol) && _roman_numerals.ContainsValue(input[i + 1].ToString()) 
-        //            && _roman_numerals.FirstOrDefault(x => x.Value == current_symbol).Key < _roman_numerals.FirstOrDefault(x => x.Value == input[i + 1].ToString()).Key)
-        //            result -= _roman_numerals.FirstOrDefault(x => x.Value == current_symbol).Key;
-        //        else
-        //            result += _roman_numerals.FirstOrDefault(x => x.Value == current_symbol).Key;
-        //    }
-        //    
-        //    return new RomanNumber { Value = result };
-        //}
-        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,36 +1,59 @@
-using APP;
+using App;
 
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        // V - 0.2
         [TestMethod]
         public void TestRomanNumberParse()
         {
             Dictionary<String, int> test_cases = new()
             {
-                { "I", 1 },
-                { "II", 2 },
-                { "III", 3 },
-                { "IV", 4 },
-                { "V", 5 },
-                { "VI", 6 },
-                { "VII", 7 },
-                { "VIII", 8 },
-                { "IX", 9 },
-                { "X", 10 },
-                { "XI", 11 },
-                { "XII", 12 },
-                { "XIII", 13 },
-                { "XIV", 14 },
-                { "XV", 15 },
-                { "XVI", 16 },
-                { "XVII", 17 },
-                { "XVIII", 18 },
-                { "XIX", 19 },
-                { "XX", 20 }
+                {"I", 1},
+                {"II", 2},
+                {"III", 3},
+                {"IIII", 4},
+                {"IV", 4},
+                {"IX", 9},
+                {"LXII", 62},
+                {"LXIII", 63},
+                {"LXIV", 64},
+                {"LXV", 65},
+                {"LXVI", 66},
+                {"LXVII", 67},
+                {"LXXXI", 81},
+                {"LXXXII", 82},
+                {"LXXXIII", 83},
+                {"LXXXIV", 84},
+                {"LXXXV", 85},
+                {"LXXXVI", 86},
+                {"V", 5},
+                {"VI", 6},
+                {"VII", 7},
+                {"VIII", 8},
+                {"VIIII", 9},
+                {"X", 10},
+                {"XI", 11},
+                {"XII", 12},
+                {"XIII", 13},
+                {"XIIII", 14},
+                {"XIIIII", 15},
+                {"XIV", 14},
+                {"XL", 40},
+                {"XLI", 41},
+                {"XLII", 42},
+                {"XLIII", 43},
+                {"XLIV", 44},
+                {"XLV", 45},
+                {"XV", 15},
+                {"XVI", 16},
+                {"XVII", 17},
+                {"XVIII", 18},
+                {"XX", 20},
+                {"XXIIIII", 25},
+                {"XXV", 25},
+                {"XXX", 30},
             };
 
             Assert.AreEqual(1, RomanNumber.Parse("I").Value, "1 == I");
@@ -58,13 +81,24 @@ namespace Tests
                 Assert.AreEqual(pair.Value, RomanNumber.Parse(pair.Key).Value, $"{pair.Value} == {pair.Key}");
         }
 
-        // V - 0.1
-        //[TestMethod]
-        //public void TestRomanNumberParse()
-        //{
-        //    Assert.AreEqual(1, RomanNumber.Parse("I").Value, "1 == I");
-        //    Assert.AreEqual(2, RomanNumber.Parse("II").Value, "2 == II");
-        //    Assert.AreEqual(5, RomanNumber.Parse("V").Value, "5 == V");
-        //}
+        [TestMethod]
+        public void TestRomanNumberParseInvalid()
+        {
+            Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse(null!), "RomanNumber.Parse(null!) -> Exeption");
+
+            var ex = Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse(""), "RomanNumber.Parse('') -> Exeption");
+
+            Assert.IsFalse(String.IsNullOrEmpty(ex.Message), "ex.Message not empty");
+
+            Dictionary<String, char> test_cases = new()
+            {
+                { "XA", 'A'},
+                { "LB", 'B'}
+            };
+
+            foreach (var pair in test_cases)
+                Assert.IsTrue(Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse(pair.Key), $"RomanNumber.Parse({pair.Key}) -> Exeption").Message
+                    .Contains($"'{pair.Value}'"), $"ex.Message contains '{pair.Value}'");
+        }
     }
 }
